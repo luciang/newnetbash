87cf27fe8f1c796c4b0c759b8e0b921fc974ad0b
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2011-01-15 03:44:21 +0200

    clone vs. unhare
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

--- newnetbash.c
+++ newnetbash.c
@@ -1,3 +1,4 @@
+#include <syscall.h>
 #include <stdio.h>
 #include <net/if.h>
 #define _GNU_SOURCE
@@ -9,9 +10,11 @@
 
 static void print_usage_exit()
 {
-  printf("Usage: newnetbash [-s|shared|-n|new]\n");
+  printf("Usage: newnetbash [-s|shared|-n|new] [-c|clone|-u|unshare]\n");
   printf("       -s|shared   launch bash in the same network namespace\n");
   printf("       -n|newnet   launch bash in a new network namespace\n");
+  printf("       -c|clone    use clone(2) before starting bash\n");
+  printf("       -u|unshare  use unshare(2) before starting bash\n");
   exit(1);
 }
 
@@ -21,18 +24,25 @@ int main(int argc, char *argv[])
    * Unshare the network namespace.
    */
 
+  int i;
   int create_new_net = 0;
+  int use_clone = 0;
 
-  if (argc != 2)
+  if (argc < 3)
     print_usage_exit();
 
-  
-  if ((strcmp(argv[1], "shared") == 0) || (strcmp(argv[1], "-s") == 0))
-    create_new_net = 0;
-  else if ((strcmp(argv[1], "newnet") == 0) || (strcmp(argv[1], "-n") == 0))
-    create_new_net = 1;
-  else
-    print_usage_exit();
+  for(i = 1; i < argc; i++) {
+    if ((strcmp(argv[i], "shared") == 0) || (strcmp(argv[i], "-s") == 0))
+      create_new_net = 0;
+    else if ((strcmp(argv[i], "newnet") == 0) || (strcmp(argv[i], "-n") == 0))
+      create_new_net = 1;
+    else if ((strcmp(argv[i], "clone") == 0) || (strcmp(argv[i], "-c") == 0))
+      use_clone = 1;
+    else if ((strcmp(argv[i], "unshare") == 0) || (strcmp(argv[i], "-u") == 0))
+      use_clone = 0;
+    else
+      print_usage_exit();
+  }
 
   if (create_new_net) {
     if (unshare(CLONE_NEWNET)) {
